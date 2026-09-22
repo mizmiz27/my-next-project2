a10ea8ab031965f7c99e52ca4a16bf82dca39b93
Masashi Mizumoto <mizumoto.web@gmail.com>
2026-09-22 22:29:39 +0900

    6章まで完了

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -35,14 +35,14 @@ export default function NewsList({ news }: Props) {
                 height={630}
               />
             )}
+            <dl className={styles.content}>
+              <dt className={styles.title}>{article.title}</dt>
+              <dd className={styles.meta}>
+                <Category category={article.category} />
+                <Date date={article.publishedAt ?? article.createdAt} />
+              </dd>
+            </dl>
           </Link>
-          <dl className={styles.content}>
-            <dt className={styles.title}>{article.title}</dt>
-            <dd className={styles.meta}>
-              <Category category={article.category} />
-              <Date date={article.publishedAt ?? article.createdAt} />
-            </dd>
-          </dl>
         </li>
       ))}
     </ul>
