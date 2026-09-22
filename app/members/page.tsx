67b997ba7aa8db1ab814c659a8ee4051bba6d76f
Masashi Mizumoto <mizumoto.web@gmail.com>
2026-09-22 16:32:54 +0900

    4章まで完了

---
+++ app/members/page.tsx
@@ -0,0 +1,72 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+const data = {
+  contents: [
+    {
+      id: "1",
+      image: {
+        url: "/img-member1.jpg",
+        width: 240,
+        height: 240,
+      },
+      name: "デイビッド・チャン",
+      position: "CEO",
+      profile:
+        "ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト",
+    },
+    {
+      id: "2",
+      image: {
+        url: "/img-member2.jpg",
+        width: 240,
+        height: 240,
+      },
+      name: "エミリー・サンダース",
+      position: "COO",
+      profile:
+        "ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト",
+    },
+    {
+      id: "3",
+      image: {
+        url: "/img-member3.jpg",
+        width: 240,
+        height: 240,
+      },
+      name: "ジョン・ウィルソン",
+      position: "CTO",
+      profile:
+        "ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト",
+    },
+  ],
+};
+
+export default function Page() {
+  return (
+    <div className={styles.container}>
+      {data.contents.length === 0 ? (
+        <p className={styles.empty}>メンバーが登録されていません。</p>
+      ) : (
+        <ul>
+          {data.contents.map((member) => (
+            <li key={member.id} className={styles.list}>
+              <Image
+                src={member.image.url}
+                alt=""
+                width={member.image.width}
+                height={member.image.height}
+                className={styles.image}
+              />
+              <dl>
+                <dt className={styles.name}>{member.name}</dt>
+                <dt className={styles.position}>{member.position}</dt>
+                <dt className={styles.profile}>{member.profile}</dt>
+              </dl>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
